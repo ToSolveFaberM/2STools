55d1e3835309404118e28301b9a1ca08cac59b4f
Gabriel Rodrigues <gabriel.rodrigues@2solve.com>
2021-03-12 15:15:37 -0300

    Add new input test

--- __tests__/packetDecode.spec.js
+++ __tests__/packetDecode.spec.js
@@ -1,58 +1,57 @@
-const { packetDecode } = require('../index.js');
+const { packetDecode } = require("../index.js");
 
 describe("packetDecode function", () => {
-  const invalidDataFormat = 'AARB2mZm';
-  const invalidOffsetFormat = '20';
-  const invalidOffsetAmount = 20;
-  const invalidOffsetNegativeAmount = -1;
-  const validData = '000441da6666';
-  const validOffset = 0;
-
-  it('should throw error \'Data format must be a hexadecimal string\' when no parameters be passed', () => {
+  it("should throw error 'Data format must be a hexadecimal string' when no parameters be passed", () => {
     try {
       packetDecode();
     } catch (e) {
-      expect(e.message).toBe('Data format must be a hexadecimal string');
+      expect(e.message).toBe("Data format must be a hexadecimal string");
     }
   });
 
-  it('should throw error \'Data format must be a hexadecimal string\'', () => {
+  it("should throw error 'Data format must be a hexadecimal string'", () => {
     try {
-      packetDecode(invalidDataFormat);
+      packetDecode("AARB2mZm");
     } catch (e) {
-      expect(e.message).toBe('Data format must be a hexadecimal string');
+      expect(e.message).toBe("Data format must be a hexadecimal string");
     }
   });
 
-  it('should throw error \'Offset format must be a positive integer number\'', () => {
+  it("should throw error 'Offset format must be a positive integer number'", () => {
     try {
-      packetDecode(validData, invalidOffsetFormat);
+      packetDecode("000441da6666", "20");
     } catch (e) {
-      expect(e.message).toBe('Offset format must be a positive integer number');
+      expect(e.message).toBe("Offset format must be a positive integer number");
     }
   });
 
-  it('should throw error \'Wrong offset value\' when invalid offset amount be passed', () => {
+  it("should throw error 'Wrong offset value' when invalid offset amount be passed", () => {
     try {
-      packetDecode(validData, invalidOffsetAmount);
+      packetDecode("000441da6666", 20);
     } catch (e) {
-      expect(e.message).toBe('Wrong offset value');
+      expect(e.message).toBe("Wrong offset value");
     }
   });
 
-  it('should throw error \'Wrong offset value\' when negative offset value be passed', () => {
+  it("should throw error 'Wrong offset value' when negative offset value be passed", () => {
     try {
-      packetDecode(validData, invalidOffsetNegativeAmount);
+      packetDecode("000441da6666", -1);
     } catch (e) {
-      expect(e.message).toBe('Wrong offset value');
+      expect(e.message).toBe("Wrong offset value");
     }
   });
 
-  it('should return decoded object from data even without offset parameter', () => {
-    expect(packetDecode(validData)).toEqual({ temp: '27.3000' });
+  it("should return decoded object from data even without offset parameter", () => {
+    expect(packetDecode("000441da6666")).toEqual({ temp: "27.3000" });
   });
 
-  it('should return decoded object from data', () => {
-    expect(packetDecode(validData, validOffset)).toEqual({ temp: '27.3000' });
+  it("should return decoded object from data", () => {
+    expect(packetDecode("000441da6666", 0)).toEqual({
+      temp: "27.3000",
+    });
+
+    expect(packetDecode("0033c1a237ac", 0)).toEqual({
+      GPS_Lat: "-20.2772",
+    });
   });
 });
